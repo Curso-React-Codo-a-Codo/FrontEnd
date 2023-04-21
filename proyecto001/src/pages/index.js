86656aa51593dcd432b931b751f5651cbc312a54
Javier Gil <javiergonzalog22@gmail.com>
2023-04-21 00:30:31 -0300

    Stage 2

--- proyecto001/src/pages/index.js
+++ proyecto001/src/pages/index.js
@@ -2,10 +2,24 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import Card  from '@/componentes/card'
+import { useEffect, useState } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  /*esto es una promesa para obtener datos desde un backend*/
+
+
+const [info, setInfo] = useState();
+  useEffect(()=>{
+     fetch("https://rickandmortyapi.com/api/character/126")
+    .then(response => response.json())
+    .then(data => setInfo([data]))
+  },[]);
+if (info === undefined){
+  return <div>Loading...</div>
+}
   return (
     <>
       <Head>
@@ -16,7 +30,12 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         Hola Mundo
-        
+        {
+          info.map(  element =>(
+            <Card key={element.name} name={element.name} img={element.image} ></Card>
+          ))
+        }
+        xd
       </main>
     </>
   )
